9bb19cc3ae4e8e08015801c40da7877a70436476
조민현 <mh@jominhyeon-ui-MacBookPro.local>
2022-03-04 14:13:49 +0900

    Create header

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import clsx from "clsx";
+import NextImage from "next/image";
+interface Props {
+  className: string;
+}
+export default function Logo({ className }: Props) {
+  return (
+    <a href="#" className={clsx("flex items-center", className)}>
+      <NextImage src="/images/converse-logo.png" width={120} height={48} />
+    </a>
+  );
+}
